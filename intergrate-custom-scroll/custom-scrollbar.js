6de66fe149d7019288984552d0ec6596f8741ed9
Mary Ogbe <maryogbe09@yahoo.com>
2018-09-04 18:06:26 +0100

    worked on understanding how the key detection works, recognizing what number belongs to what keyCode

--- intergrate-custom-scroll/custom-scrollbar.js
+++ intergrate-custom-scroll/custom-scrollbar.js
@@ -1,8 +1,18 @@
-$( "h1" ).clone().appendTo( document.body );
-$( "h1" ).clone().appendTo( document.body );
-$( "h1" ).clone().appendTo( document.body );
-$( "h1" ).clone().appendTo( document.body );
-$( window ).scroll(function() {
-  $( "span" ).css( "display", "inline" );
-  $( "span" ).fadeOut( "slow" );
-});
+// $( "h1" ).clone().appendTo( document.body );
+// $( "h1" ).clone().appendTo( document.body );
+// $( "h1" ).clone().appendTo( document.body );
+// $( "h1" ).clone().appendTo( document.body );
+// $( window ).scroll(function() {
+//   $( "span" ).css( "display", "inline" );
+//   $( "span" ).fadeOut( "slow" );
+// });
+
+(function($){
+  $(window).on("load", function(){
+    alert('hello');
+    $(".CustomScrollbar").mCustomScrollerbar({
+      
+      //axis:"x";
+    });
+  });
+})(jquery);
--- key-dectection/index.js
+++ key-dectection/index.js
@@ -2,12 +2,19 @@ function checkPhoneKey(key) {
     return (key >= '0' && key <= '9') || key == '+' || key == '(' || key == ')' || key == '-';
   }
 
-function keyCode(event){
-    var w = event.keyCode;
-    var text001;
-    var b =input001.value;
-    if( w == 13 && b == "a - z"){
-        text001="hello" + ('count ++');
-        document.getElementById("text-entered").innerHTML= text001;
-}
+  document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 13:
+        //  alert('left');
+         document.querySelector("#text-entered").innerHTML = "The last key Entered: " +  '' + e.which || '';
+        $("p").css("color","hotpink");
+
+        break;
+        case 32:
+        document.querySelector("p").innerHTML = "The last key Entered: " +  '' + e.which || '';
+            break;
+        case 9:
+        document.querySelector("p").innerHTML = "The last key Entered: " +  '' + e.which || '';
+            break;
+    }
 }
