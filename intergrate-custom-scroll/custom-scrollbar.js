c5c42f405f62b9a06049e95d145234c62b2ced55
Mary Ogbe <maryogbe09@yahoo.com>
2018-09-04 12:31:04 +0100

    custom-scrollbar

---
+++ intergrate-custom-scroll/custom-scrollbar.js
@@ -0,0 +1,8 @@
+$( "h1" ).clone().appendTo( document.body );
+$( "h1" ).clone().appendTo( document.body );
+$( "h1" ).clone().appendTo( document.body );
+$( "h1" ).clone().appendTo( document.body );
+$( window ).scroll(function() {
+  $( "span" ).css( "display", "inline" );
+  $( "span" ).fadeOut( "slow" );
+});
