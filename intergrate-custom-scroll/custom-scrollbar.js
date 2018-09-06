051a5910c06dd2b4d8c7c2fba3aa2f4c675aca37
Mary Ogbe <maryogbe09@yahoo.com>
2018-09-06 17:59:52 +0100

    added new link got scrollbar working

--- intergrate-custom-scroll/custom-scrollbar.js
+++ intergrate-custom-scroll/custom-scrollbar.js
@@ -1,14 +1,5 @@
-$( "h1" ).clone().appendTo( document.body );
-$( "h1" ).clone().appendTo( document.body );
-$( "h1" ).clone().appendTo( document.body );
-$( "h1" ).clone().appendTo( document.body );
-$( window ).scroll(function() {
-  $( "span" ).css( "display", "inline" );
-  $( "span" ).fadeOut( "slow" );
-});
-
 (function($){
   $(window).on("load",function(){
-      $("body").mCustomScrollbar();
+    $(".content").mCustomScrollbar();
   });
 })(jQuery);
--- select-manager/passenger-details.js
+++ select-manager/passenger-details.js
@@ -1,9 +1,4 @@
-function updateDetails(){
-    // alert("hello");   
-// $("age").html("<select name='select-picker 'id='age' class='selectPicker'> <option value='mr'>0-3</option><option value='mrs>3-15</option> <option value='ms'>18+</option></select>");
-if(){
-        $("#age").on("click", function(){
-        $("#age").show();
-        });
-    }
-}
+var button= $(".btn").on('click', function(){
+    $("#update").html("<select name='select-picker class='selectPicker' id='age'> <option value='0'>0-3</option><option value='3'>3-15</option> <option value='18'>18+</option></select>");
+    $('select option[value=3]').prop('selected', true);
+}) 
