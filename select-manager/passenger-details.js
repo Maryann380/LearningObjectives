3eb4657e54e48ec5737c50bcd8186a4f51748910
Mary Ogbe <maryogbe09@yahoo.com>
2018-09-06 11:08:52 +0100

    cleaning up code

--- select-manager/passenger-details.js
+++ select-manager/passenger-details.js
@@ -1,9 +1,9 @@
-function updateDetails(){
+$(document).ready(function updateDetails(){
     
 var details = function(){
-     document.getElementById("update").innerHTML = "<select name='select-picker class='selectPicker' id='age'> <option value='0'>0-3</option><option value='3'>3-15</option> <option value='18'>18+</option></select>;"
-     $('#age').val("3-15");
+     $("#update").html("<select name='select-picker class='selectPicker' id='age'> <option value='0'>0-3</option><option value='3'>3-15</option> <option value='18'>18+</option></select>");
+     $('select option[value=3]').prop('selected', true);
 }
 
 details();
-}
+});
