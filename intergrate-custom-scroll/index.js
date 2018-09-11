5897421fd8bd57436f2d85883c7b2357d94e4d6d
Mary Ogbe <maryogbe09@yahoo.com>
2018-09-11 11:03:45 +0100

    working on the scrollball trying different styles

--- intergrate-custom-scroll/index.js
+++ intergrate-custom-scroll/index.js
@@ -1,5 +1,45 @@
-(function($){
-  $(window).on("load",function(){
-    $(".content").mCustomScrollbar();
-  });
-})(jQuery);
+
+// (function($){
+//   $(window).on("load",function(){
+//     // $(".register-form").mCustomScrollbar({
+//     //   axis:"xy",
+//     //   theme:"dark-3",
+//     //   advanced:{
+//     //     autoExpandHorizontalScroll:2
+//     //   },
+//     //   callbacks:{
+//     //     onCreate:function(){
+//     //       $(this).find("p").css("width",$(this).width());
+//     //     },
+//     //     onBeforeUpdate:function(){
+//     //       $(this).find("p").css("width",$(this).width());
+//     //     }
+//     //   }
+//     // });
+//     alert('hello');
+//     document.getElementById(".register-form").style.overflow='auto';
+
+//   });
+// })(jQuery);
+
+// // document.getElementById(".register-form").style.overflow='auto';
+// // alert('hello');
+// // (function($){
+// //   $(window).on("load",function(){
+// //     $(".mcs-horizontal-example").mCustomScrollbar({
+// //       axis:"y",
+// //       theme:"dark-3",
+// //       advanced:{
+// //         autoExpandHorizontalScroll:2
+// //       },
+// //       callbacks:{
+// //         onCreate:function(){
+// //           $(this).find("p").css("width",$(this).width());
+// //         },
+// //         onBeforeUpdate:function(){
+// //           $(this).find("p").css("width",$(this).width());
+// //         }
+// //       }
+// //     });
+// //   });
+// // })(jQuery);
