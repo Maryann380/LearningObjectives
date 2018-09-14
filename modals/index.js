fbc836cc81899929437e3cb355d26bf2179ce504
Mary Ogbe <maryogbe09@yahoo.com>
2018-09-14 17:48:40 +0100

    worked on modal task

--- modals/index.js
+++ modals/index.js
@@ -1,20 +1,6 @@
-var modal = document.getElementsByClassName('simpleModal');
-// var modalBtn = document.getElementsByClassName('modalBtn');
-var closeBtn = document.getElementsByClassName('closeBtn');
 
-modalBtn.addEventListener('click',openModal);
-// closeBtn.addEventListener('click',closeModal);
-window.addEventListener("click", outsideClick);
-
-//opens modal
-function openModal(){
-    modal.style.display = 'block';
-   
-}
-//close modal
-function closeModal(){
-    modal.style.display = 'none';
-}
-function outsideClick(){
-    modal.style.display = 'block';
-}
+$(function () { 
+    $('#myModal').modal({
+    keyboard: true
+    })
+});
