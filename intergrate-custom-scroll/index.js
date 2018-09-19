02715ac95f0b6ff03111c89f2558f9d278799cfc
Mary Ogbe <maryogbe09@yahoo.com>
2018-09-19 11:17:17 +0100

    attempting different routes to create a mcustomScrollbar

--- intergrate-custom-scroll/index.js
+++ intergrate-custom-scroll/index.js
@@ -1,50 +1,32 @@
-
 // (function($){
-//   $(window).on("load",function(){
-//     // $(".register-form").mCustomScrollbar({
-//     //   axis:"xy",
-//     //   theme:"dark-3",
-//     //   advanced:{
-//     //     autoExpandHorizontalScroll:2
-//     //   },
-//     //   callbacks:{
-//     //     onCreate:function(){
-//     //       $(this).find("p").css("width",$(this).width());
-//     //     },
-//     //     onBeforeUpdate:function(){
-//     //       $(this).find("p").css("width",$(this).width());
-//     //     }
-//     //   }
-//     // });
-//     alert('hello');
-//     document.getElementById(".register-form").style.overflow='auto';
-
-//   });
+//     $(window).on("load",function(){
+//         $(".mCustomScrollbar").mCustomScrollbar({
+//             axis:"xy"
+//         });
+//     });
 // })(jQuery);
 
-//  document.getElementById(".register-form").style.overflow='auto';
-//  alert('hello');
-//  (function($){
-//    $(window).on("load",function(){
-//      $(".mcs-horizontal-example").mCustomScrollbar({
-//        axis:"y",
-//        theme:"dark-3",
-//        advanced:{
-//          autoExpandHorizontalScroll:2
-//        },
-//        callbacks:{
-//          onCreate:function(){
-//            $(this).find("p").css("width",$(this).width());
-//          },
-//          onBeforeUpdate:function(){
-//            $(this).find("p").css("width",$(this).width());
-//          }
-//        }
-//      });
-//    });
-//  })(jQuery);
-(function($){
-    $(window).on("load",function(){
-        $(".content").mCustomScrollbar();
-    });
-})(jQuery);
+// $(document).ready(function(){
+//     $bHeight =$('body').height();
+//     $bHeight =$('.scrollBar').height();
+//     $sliderHeight = $sliderHeight/$bHeight*100;
+//     $(".slider").height($sliderHeight + '%');
+
+//     $(".slider").draggable({
+//         containment:'parent',
+//         axis: 'xy',
+//         drag: function(){
+//             $pos = $(".slider").position().top;
+//             $ScrollPercent= $pos/$sHeight*100;
+//             $ScrollPx = $ScrollPercent/100*$bHeight;
+//             $('body').scrollTop('$ScrollPx');
+//         }
+
+//     })
+    
+// })
+
+        $(window).on("load",function(){
+            $(".register-form").mCustomScrollbar();
+        });
+ 
