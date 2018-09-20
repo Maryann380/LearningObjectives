5a20b87ed7fe74d2e17aea66639d5ed5a041497b
Mary Ogbe <maryogbe09@yahoo.com>
2018-09-20 13:45:11 +0100

    took of commented code

--- intergrate-custom-scroll/index.js
+++ intergrate-custom-scroll/index.js
@@ -1,8 +1,3 @@
-// $( document ).ready(function() {
-//     $(".content").mCustomScrollbar();
-//     $(".content.mCustomScrollbar").css("background", "yellow");
-// });
-
 (function($){
     $(window).on("load",function(){
         $(".content").mCustomScrollbar();
