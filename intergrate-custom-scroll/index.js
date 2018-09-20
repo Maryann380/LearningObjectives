7558cd558b0602e6d843e11edc36701a8728492b
Mary Ogbe <maryogbe09@yahoo.com>
2018-09-20 11:24:47 +0100

    working on custom Scrollbar

--- intergrate-custom-scroll/index.js
+++ intergrate-custom-scroll/index.js
@@ -1,37 +1,5 @@
-// (function($){
-//     $(window).on("load",function(){
-//         $(".mCustomScrollbar").mCustomScrollbar({
-//             axis:"xy"
-//         });
-//     });
-// })(jQuery);
-
-// $(document).ready(function(){
-//     $bHeight =$('body').height();
-//     $bHeight =$('.scrollBar').height();
-//     $sliderHeight = $sliderHeight/$bHeight*100;
-//     $(".slider").height($sliderHeight + '%');
-
-//     $(".slider").draggable({
-//         containment:'parent',
-//         axis: 'xy',
-//         drag: function(){
-//             $pos = $(".slider").position().top;
-//             $ScrollPercent= $pos/$sHeight*100;
-//             $ScrollPx = $ScrollPercent/100*$bHeight;
-//             $('body').scrollTop('$ScrollPx');
-//         }
-
-//     })
-    
-// })
-
-        // $(window).on("load",function(){
-        //     $(".register-form").mCustomScrollbar();
-        // });
- 
-
 $( document ).ready(function() {
-    $(".register-detail").mCustomScrollbar();
+    $(".content").mCustomScrollbar();
+    $(".content.mCustomScrollbar").css("background", "yellow");
 });
 
