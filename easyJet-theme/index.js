78d02eeb2c4c4d61a9e2bb34e84a3e4692be3012
Mary Ogbe <maryogbe09@yahoo.com>
2018-10-01 15:12:38 +0100

    fixed calender

--- easyJet-theme/index.js
+++ easyJet-theme/index.js
@@ -1,68 +1,125 @@
 $(document).ready(function(){
-    $('.dropdown-toggle').dropdown();
-    $('#datetimepickerDeparture').datetimepicker();
-        $('#datetimepickerTo').datetimepicker({
-            useCurrent: false
-        });
-            $("#datetimepickerDeparture").on("dp.change", function(e) {
-            $('#datetimepickerTo').data("DateTimePicker").minDate(e.date);
-        });
-            $("#datetimepickerTo").on("dp.change", function(e) {
-            $('#datetimepickerDeparture').data("DateTimePicker").maxDate(e.date);
-        });
-    });
-    $(document).ready(function() {
+ $('.dropdown-toggle').dropdown();
+ 
+// function submit(){
+//     function createNode(element) {
+//         return document.createElement(element);
+//     }
+    
+//     function append(parent, el) {
+//       return parent.appendChild(el);
+//     }
+    
+//     const ul = document.getElementById('#country');
+//     const url = 'https://gist.githubusercontent.com/Maryann380/ead39e3ed34bbfb619a3acd07a4682c4/raw/21124a369f57b489c1102e11f40be787bb732d33/country1.json';
+//     fetch(url)
+    
+//     .then((resp) => resp.json())
+//     .then(function(data) {
+//         debugger;
+//       let airports = data;
+//       return airports.map(function(country) {
+//       })
+//     })
+// }
+var substringMatcher = function(strs) {
+    return function findMatches(q, cb) {
+      var matches, substringRegex;
+  
+      // an array that will be populated with substring matches
+      matches = [];
+  
+      // regex used to determine if a string contains the substring `q`
+      substrRegex = new RegExp(q, "i");
+  
+      // iterate through the pool of strings and for any string that
+      // contains the substring `q`, add it to the `matches` array
+      $.each(strs, function(i, str) {
+        if (substrRegex.test(str)) {
+          matches.push(str);
+        }
+      });
+  
+      cb(matches);
+    };
+  };
+  
+  var states = [
+    "Alabama",
+    "Alaska",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "Florida",
+    "Georgia",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Vermont",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming"
+  ];
+  
+  $(document).ready(function() {
+    $("#country .typeahead").typeahead(
+      {
+        hint: true,
+        highlight: true,
+        minLength: 1
+      },
+      {
+        name: "states",
+        source: substringMatcher(states)
+      }
+    );
+  });  
+})
 
-        $('#calendar').fullCalendar({
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
-            defaultDate: '2016-09-12',
-            navLinks: true, // can click day/week names to navigate views
-            selectable: true,
-            selectHelper: true,
-            select: function(start, end) {
-                // Display the modal.
-                // You could fill in the start and end fields based on the parameters
-                $('.modal').modal('show');
+// $('.dropdown-toggle').dropdown();
 
-            },
-            eventClick: function(event, element) {
-                // Display the modal and set the values to the event values.
-                $('.modal').modal('show');
-                $('.modal').find('#title').val(event.title);
-                $('.modal').find('#starts-at').val(event.start);
-                $('.modal').find('#ends-at').val(event.end);
-
-            },
-            editable: true,
-            eventLimit: true // allow "more" link when too many events
-
-        });
-
-        // Bind the dates to datetimepicker.
-        // You should pass the options you need
-        $("#starts-at, #ends-at").datetimepicker();
-
-        // Whenever the user clicks on the "save" button om the dialog
-        $('#save-event').on('click', function() {
-            var title = $('#title').val();
-            if (title) {
-                var eventData = {
-                    title: title,
-                    start: $('#starts-at').val(),
-                    end: $('#ends-at').val()
-                };
-                $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
-            }
-            $('#calendar').fullCalendar('unselect');
-
-            // Clear modal inputs
-            $('.modal').find('input').val('');
-
-            // hide modal
-            $('.modal').modal('hide');
-        });
-    });
+$('#datetimepickerFrom').Zebra_DatePicker({
+  direction: 1,
+  pair: $('.modal-body .form-group #datetimepickerTo'),
+	always_visible: $('#myModal .form-group #datetimepickerFrom')
+});
+$('#datetimepickerTo').Zebra_DatePicker({
+  direction: 2, 
+  always_visible: $('#myModal .form-group #datetimepickerTo')
+});
